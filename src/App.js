99b87e8082d9293ade134fe6fee60385f48162e1
Serena Celeste <serenaceleste@gmail.com>
2021-04-10 14:35:09 -0400

    Configuring React

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import logo from "./logo.svg";
+import React, { Component } from 'react';
+import { Navbar, NavbarBrand } from 'reactstrap';
 import "./App.css";
 
 function App() {
@@ -7,7 +9,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload
+          Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
